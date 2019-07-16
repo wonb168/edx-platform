d5f225dacf4c3c8b0f03f48bb9f8d8ccfac932eb
Calen Pennington <cale@edx.org>
2019-07-16 13:30:59 -0400

    fixup! Add django app for running Coverage who-tests-what in bokchoy

--- openedx/testing/coverage_context_listener/views.py
+++ openedx/testing/coverage_context_listener/views.py
@@ -1,6 +1,8 @@
 import coverage
+from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+
 
 @require_POST
 @csrf_exempt
@@ -9,5 +11,6 @@ def update_context(request):
     when = request.POST.get('when')
     current = coverage.Coverage.current()
     if current is not None and item and when:
-        context = "{item.nodeid}|{when}".format(item=item, when=when)
+        context = "{item}|{when}".format(item=item, when=when)
         current.switch_context(context)
+    return HttpResponse(status=204)
